e194d264a835b8d1aec4de9e7b5885bf957f664e
Jose Luis Santos Montiel <ingsantjose@gmail.com>
2018-10-09 00:47:37 -0500

    llave y usuario como variables.

--- adduser.sh
+++ adduser.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 read -p "Nombre de usuario: " usuario
-echo " "\ &&
-echo "Este script asume que existe el grupo (operaciones)"
+read -p "Llave publica ssh: " llave
+echo " Ejecutando "
 
-llave="EstoEsUnaLlaveInsegura"
+groupadd operaciones
 useradd -m -g operaciones ${usuario}
 mkdir -p /home/${usuario}/.ssh/
 touch /home/${usuario}/.ssh/authorized_keys
@@ -13,3 +13,5 @@ chmod 600 /home/${usuario}/.ssh/authorized_keys
 echo ${llave} > /home/${usuario}/.ssh/authorized_keys
 chown -R ${usuario} /home/${usuario}/
 echo ${usuario}'  ALL=(ALL)      NOPASSWD: ALL' >> /etc/sudoers
+
+echo " Script finalizado "
